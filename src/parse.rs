62990be75ba2c54e6e2b204f2ae60470f0f5e779
Josh Driver <keeperofdakeys@gmail.com>
2014-09-03 22:10:40 +0930

    Implementing more parsing.

--- src/parse.rs
+++ src/parse.rs
@@ -7,7 +7,7 @@ enum ParseError {
 
 fn parse_statement( tokens: Vec<Token> ) -> Result<(), ParseError> {
   let mut paren_nest = 0u;
-  let command = match parse_command( &tokens ) {
+  let command = match parse_command( tokens.as_slice() ) {
     Some( c ) => c,
     None => return Err( ErrNoCommand )
   };
@@ -20,14 +20,45 @@ enum Command {
   CmdUpdate
 }
 
-fn parse_command( tokens: &Vec<Token> ) -> Option<Command> {
+fn parse_command( tokens: &[Token] ) -> Option<Command> {
   if tokens.len() == 0 {
     return None;
   }
-  Some( match tokens[0].get_token() {
-    "select" => CmdSelect,
-    "insert" => CmdInsert,
-    "update" => CmdUpdate,
-    _ => return None
-  } )
+  match tokens[0].get_token() {
+    "select" => parse_select( tokens.slice_from( 1 ) ),
+    "insert" => parse_insert( tokens.slice_from( 1 ) ),
+    "update" => parse_update( tokens.slice_from( 1 ) ),
+    _ => None
+  }
+}
+
+macro_rules! advance_iter(
+  ($iter: ident, $index: ident, $val: ident) => (
+    match $iter.next() {
+      Some( (i, v) ) => {
+        $index = i;
+        $val = v;
+      }
+      None => break
+    }
+  )
+)
+    
+
+fn parse_select( tokens: &[Token] ) -> Option<Command> {
+  let mut values = Vec::new();
+  let mut iter = tokens.iter().enumerate();
+  let (index, val): (uint, &Token);
+  loop {
+    advance_iter!( iter, index, val );
+  }
+  None
+}
+
+fn parse_insert( tokens: &[Token] ) -> Option<Command> {
+  None
+}
+
+fn parse_update( tokens: &[Token] ) -> Option<Command> {
+  None
 }
